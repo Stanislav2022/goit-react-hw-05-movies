135a0ae3a8d06008876aafc030838e532c223fec
Stanislav2022 <tsv5@ukr.net>
2022-10-30 22:57:35 +0200

    1

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+      <nav>
+            <div>
+                <Link to='/'>Logo</Link>
+            </div>    
+      </nav>
+
+  )
+}
