0e4933086fc28f0b9f45e2110be29cb42fc85bf1
Stanislav2022 <tsv5@ukr.net>
2022-10-31 18:23:47 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from './pages/HomePage/Home'
 import Movies from './pages/MoviesPage/Movies'
+import Navbar from "components/Navbar/Navbar";
 
 export const App = () => {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies/> } />
