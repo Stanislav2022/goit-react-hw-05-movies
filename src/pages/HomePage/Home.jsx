9c2277de8aea2a1bbb21694bf7b07c90f0102ba7
Stanislav2022 <tsv5@ukr.net>
2022-11-01 21:44:57 +0200

    1

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import TrandingList from '../../components/TrandingList/TrandingList'
 
 export default function Home() {
+
+
   return (
     <div>
-
+      <h3>Tranding today</h3>
+      <TrandingList/>
     </div>
   )
 }
