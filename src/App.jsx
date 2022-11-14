64d840591a9e727790f22c8a669ba56a027fae55
Stanislav2022 <tsv5@ukr.net>
2022-11-14 23:08:28 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
 import { Route, Routes } from "react-router-dom";
-import Home from './pages/HomePage/Home'
-import Movies from './pages/MoviesPage/Movies'
-import Navbar from "components/Navbar/Navbar";
-import MovieDetails from "./pages/MovieDetailsPage/MovieDetails"
-import Cast from "pages/MovieDetailsPage/Cast/Cast";
-import Reviews from "pages/MovieDetailsPage/Reviews/Reviews";
+import { lazy, Suspense } from "react";
+const Home = lazy(() => import('./pages/HomePage/Home'), {});
+const Movies = lazy(() => import('./pages/MoviesPage/Movies'));
+const Navbar = lazy(() => import('components/Navbar/Navbar'));
+const MovieDetails = lazy(() => import('./pages/MovieDetailsPage/MovieDetails'));
+const Cast = lazy(() => import('pages/MovieDetailsPage/Cast/Cast'));
+const Reviews = lazy(() => import('pages/MovieDetailsPage/Reviews/Reviews'));
+
 
 export const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/movies/:id" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />           
-        </Route>
-        <Route path="*" element={<Home />} />
-      </Routes>
+      <Navbar />
+      <Suspense>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/movies/:id" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />           
+          </Route>
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
