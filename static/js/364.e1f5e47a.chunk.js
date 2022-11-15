cd4b897905d90d58456369f8dd24c16ef5c836af
Stanislav2022 <96731862+Stanislav2022@users.noreply.github.com>
2022-11-15 17:13:22 +0000

    Deploying to gh-pages from @ Stanislav2022/goit-react-hw-05-movies@dd2ea7357b91b4d30cb9a9d7b6a19aaaf8da214f 🚀

---
+++ static/js/364.e1f5e47a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[364],{364:function(e,t,r){r.r(t);r(791);var n=r(87),o=r(184);t.default=function(){return(0,o.jsxs)("div",{children:[(0,o.jsx)("h1",{children:"404 - Not Found!"}),(0,o.jsx)(n.rU,{to:"/",children:"Go Home"})]})}}}]);
+//# sourceMappingURL=364.e1f5e47a.chunk.js.map
