16f0f2e746e7453f46e18d638d0a4ad8ad45a040
Stanislav2022 <tsv5@ukr.net>
2022-11-15 19:09:02 +0200

    1

--- src/pages/MovieDetailsPage/MovieDetails.jsx
+++ src/pages/MovieDetailsPage/MovieDetails.jsx
@@ -1,8 +1,9 @@
-import React, { useState , useEffect} from 'react'
+import React, { useState, useEffect, Suspense } from 'react';
 import Loader from 'components/Loader/Loader';
-import {useParams, useNavigate, NavLink, Outlet } from 'react-router-dom';
+import {useParams, Outlet, useLocation,Link } from 'react-router-dom';
 import { getMovieDetails } from 'services/api';
 import style from "./MovieDetails.module.css";
+
 
 export default function MovieDetails() {
     const [state, setState] = useState(null);
@@ -10,8 +11,6 @@ export default function MovieDetails() {
     const [error, setError] = useState(null);
 
     const { id } = useParams();
-    const navigate = useNavigate();
-
     
     useEffect(() => {
         const fetchMovie = async () => {
@@ -59,23 +58,24 @@ export default function MovieDetails() {
         }
         return `0`;
     }
-    
-    const goBack = () => { navigate("/") }; 
+    const location = useLocation;
+    const cameBack = location.state?.from ?? '/';
+
 
 return (
     <div>
-        <button on onClick={goBack}>Go back</button>
+        <Link to={cameBack}>Go back</Link>
         {loading && <Loader />}
         {error && <p>Something didn't go according to plan</p>}
         {state && (
             <>
                 <div className={style.section}>
                     <img className={style.films__img} loading="lazy" src={createPosterFilm(state.poster_path)} alt={state.title} />
-                    <div>
+                    <div className={style.description}>
                         <h1>{state.title} ({createYearFilm(String(state.release_date).slice(0, 4))})</h1>
                         <p>User Score {createRating(state.vote_average)}%</p>
                         <h2>Overview</h2>
-                        <p>{state.overview}</p>
+                        <p className={style.overview}>{state.overview}</p>
                         <h2>Ganres </h2>
                         <p>{createGenresFilmArray(state.genres)}</p>
                     </div>
@@ -83,12 +83,14 @@ return (
                 <div className={style.section__border}>
                     <h3>Additional information</h3>
                     <ul>
-                        <li><NavLink to={'cast'}>Cast</NavLink></li>
-                        <li><NavLink to={'reviews'}>Reviews</NavLink></li>
+                        <li><Link to={'cast'} state={{from:cameBack}}>Cast sat</Link></li>
+                        <li><Link to={'reviews'} state={{from:cameBack}}>Reviews</Link></li>
                     </ul>
                 </div>
                 <div >
-                    <Outlet/>
+                    <Suspense fallback={<div>Loading...</div>}>
+                        <Outlet/>
+                    </Suspense>
                 </div>
             </>
         )}
